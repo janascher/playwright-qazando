56de799d769d3b8d9cb3eee8388cdecbd3c40abd
janascher <janainascher@protonmail.com>
2024-09-13 12:40:07 -0300

    feat: screenshots

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,5 +1,5 @@
 // @ts-check
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 
 test('Login com sucesso', async ({ page }) => {
     await page.goto('https://automationpratice.com.br/');
@@ -9,7 +9,10 @@ test('Login com sucesso', async ({ page }) => {
     await page.locator('#user').fill('teste@teste.com');
     await page.locator('#password').click();
     await page.locator('#password').fill('123456');
+    await page.locator('#password').screenshot({ path: 'screenshots/login_com_sucesso/elemento_senha.png' }); // Captura um elemento específico
+    await page.screenshot({ path: 'screenshots/login_com_sucesso/campos_preenchidos.png' }); // Captura a tela da página inicial
     await page.getByRole('button', { name: 'login' }).click();
     await page.getByRole('heading', { name: 'Login realizado' }).click();
     await page.getByText('Olá, teste@teste.com').click();
+    await page.screenshot({ path: 'screenshots/login_com_sucesso/login_realizado.png' }); // Captura a tela da página inicial
 });
