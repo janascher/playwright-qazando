06d5efe07cd9f4741dbb8cde26cad6e667311d4b
janascher <janainascher@protonmail.com>
2024-11-19 11:12:30 -0300

    chore: adicionar workflow para rodar testes no push

--- tests/login_page_screenchot.spec.js
+++ tests/login_page_screenchot.spec.js
@@ -1,24 +1,11 @@
 // @ts-check
 import { test } from '@playwright/test';
 
-test('Login com sucesso @login', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
     await page.goto('https://automationpratice.com.br/');
-    await page.getByRole('link', { name: ' Login' }).click();
-    await page.getByRole('heading', { name: 'Login' }).click();
-    await page.locator('#user').click();
-    await page.locator('#user').fill('teste@teste.com');
-    await page.locator('#password').click();
-    await page.locator('#password').fill('123456');
-    await page.locator('#password').screenshot({ path: 'screenshots/login_com_sucesso/elemento_senha.png' }); // Captura um elemento específico
-    await page.screenshot({ path: 'screenshots/login_com_sucesso/campos_preenchidos.png' }); // Captura a tela da página inicial
-    await page.getByRole('button', { name: 'login' }).click();
-    await page.getByRole('heading', { name: 'Login realizado' }).click();
-    await page.getByText('Olá, teste@teste.com').click();
-    await page.screenshot({ path: 'screenshots/login_com_sucesso/login_realizado.png' }); // Captura a tela da página inicial
 });
 
-test('Login com sucesso 1', async ({ page }) => {
-    await page.goto('https://automationpratice.com.br/');
+test('Login com sucesso - Captura de tela @login', async ({ page }) => {
     await page.getByRole('link', { name: ' Login' }).click();
     await page.getByRole('heading', { name: 'Login' }).click();
     await page.locator('#user').click();
@@ -35,8 +22,7 @@ test('Login com sucesso 1', async ({ page }) => {
     await page.screenshot({ path: 'screenshots/login_com_sucesso/login_realizado.png' }); // Captura a tela da página inicial
 });
 
-test('Login com sucesso 2 @login', async ({ page }) => {
-    await page.goto('https://automationpratice.com.br/');
+test('Login com sucesso - Captura o campo senha @login', async ({ page }) => {
     await page.getByRole('link', { name: ' Login' }).click();
     await page.getByRole('heading', { name: 'Login' }).click();
     await page.locator('#user').click();
@@ -51,4 +37,8 @@ test('Login com sucesso 2 @login', async ({ page }) => {
     await page.getByRole('heading', { name: 'Login realizado' }).click();
     await page.getByText('Olá, teste@teste.com').click();
     await page.screenshot({ path: 'screenshots/login_com_sucesso/login_realizado.png' }); // Captura a tela da página inicial
+});
+
+test.afterEach(async ({ page }) => {
+    // Seu código de finalização aqui
 });
